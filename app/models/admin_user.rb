4e772a33b609c391b03536ad4df51e1c3a2ea207
Alexander Costa <ajc458@yahoo.com>
2013-03-08 22:35:00 -0500

    Finishing attr_accessible in models

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -6,7 +6,7 @@ class AdminUser < ActiveRecord::Base
   has_many :section_edits
   has_many :sections, :through => :section_edits
   
-  attr_accessible :first_name, :last_name, :email, :hashed_password, :username
+  attr_accessible :first_name, :last_name, :email, :hashed_password, :username, :salt
   
   scope :named, lambda {|first,last| where(:first_name => first, :last_name => last)}
   
