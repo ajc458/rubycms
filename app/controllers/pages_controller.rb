69011b8a06f8560b0ab3e52fccc9b21db51e4745
Alexander Costa <ajc458@yahoo.com>
2013-03-08 22:54:13 -0500

    added pages controller and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,66 @@
 class PagesController < ApplicationController
+  
+  layout 'admin'
+  
+  def index
+    list
+    render('list')
+  end
+  
+  def list
+    @pages = Page.order('pages.position ASC')
+  end
+  
+  def show
+    @page = Page.find(params[:id])
+  end
+  
+  def new
+    @page = Page.new(:name => 'default')
+  end
+  
+  def create
+    # instantiate a new object using form parameters
+    @page = Page.new(params[:page])
+    # save object
+    if @page.save
+      # if success, redirect to list action
+      flash[:notice] = 'Page created.'
+      redirect_to(:action => 'list')
+    else
+      # if fails, redisplay form
+      render('new')
+    end
+  end
+  
+  def edit
+    @page = Page.find(params[:id])
+  end
+  
+  def update
+    # find object using form parameters
+    @page = Page.find(params[:id])
+    # update the object
+    if @page.update_attributes(params[:page])
+      # if success, redirect to list action
+      flash[:notice] = 'Page updated.'
+      redirect_to(:action => 'show', :id => @page.id)
+    else
+      # if fails, redisplay form
+      render('edit')
+    end
+  end
+  
+  def delete
+    @page = Page.find(params[:id])
+  end
+  
+  def destroy
+    # destroy object
+    Page.find(params[:id]).destroy
+    # redirect
+    flash[:notice] = 'Page destroyed.'
+    redirect_to(:action => 'list')
+  end
+  
 end
